72b63695185d3333653afb52c8870027a4502c49
0sscar <gosg.gabriel@hotmail.com>
2025-05-23 00:43:23 -0300

    criacao da tabela
    tabela criada (ainda incompleta)

--- CRUD_interface/interface.py
+++ CRUD_interface/interface.py
@@ -1,10 +1,13 @@
 from tkinter import* 
+from tkinter import ttk 
+
 
 janela = Tk() 
 janela.geometry("1043x593")
 janela.configure(bg="#0C0C0C")
 janela.resizable(width=FALSE, height=FALSE)#janela fixa
-
+#janela.grid_rowconfigure(0, weight=1)
+#janela.grid_columnconfigure(1, weight=1)
 
 #divisão de elementos 
 frame1 = Frame(janela, width = 410, height=600, bg="#FFFFFF")
@@ -49,4 +52,41 @@ botao_apagar.place(x=155, y=540)
 
 botao_atualizar = Button(frame1,width=10, height=1, text="Atualizar",anchor=CENTER,fg="#FFFFFF", bg="#5372FF",font=("Ivy 10 bold"), relief= "raised", overrelief="ridge" )
 botao_atualizar.place(x=260, y=540)
-janela.mainloop()  
+
+
+
+#criacao da tabela 
+
+
+
+
+campos = ["ID","Nome","Email", "Disciplina"] 
+
+tabela =  ttk.Treeview(frame2, selectmode="extend",columns=campos, show="headings",height=15,style="Custom.Treeview") 
+tabela.place(x=10, y=10, width=620, height=580)
+vscroll = ttk.Scrollbar(frame2, orient="vertical", command=tabela.yview) 
+
+hscroll = ttk.Scrollbar(frame2, orient="horizontal", command=tabela.xview) 
+
+tabela.configure(yscrollcommand=vscroll.set, xscrollcommand=hscroll.set)
+
+#posicionando elementos  
+tabela.grid(column=0,row=0,sticky="nsew") 
+vscroll.grid(column=1,row=0,sticky="ns") 
+hscroll.grid(column=0,row=1,sticky="ew") 
+
+frame2.grid_rowconfigure(0, weight=1)
+frame2.grid_columnconfigure(1, weight=1)
+
+header = ["nw","nw", "nw","nw"] 
+header_size=[30,170,140,100] 
+n=0
+
+for i in campos:
+    tabela.heading(i, text=i.title(), anchor=CENTER) 
+    tabela.column(i,width=header_size[n],anchor=header[n]) 
+
+    n+=1 
+
+
+janela.mainloop()      
